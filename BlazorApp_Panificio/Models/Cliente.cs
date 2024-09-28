048f5c33c30f5c2c2d87c73372f00f665164c1d1
Marcxzz <angionimarco2.0@gmail.com>
2024-09-28 10:46:14 +0200

    Aggiungere i file di progetto.

---
+++ BlazorApp_Panificio/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp_Panificio.Models
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public string? nome { get; set; }
+        public string? cognome { get; set; }
+        public double quantitaPaneGiornaliero { get; set; }
+        public string? paese { get; set; }
+        public string? telefono { get; set; }
+    }
+}
